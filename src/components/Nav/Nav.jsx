af3b644f4135da7b12f03da46b943cbf01221424
Emily Engel <emilyengel@Emilys-iMac.local>
2020-08-19 12:32:13 -0400

    minor changes to nav styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import UserAvatar from '../UserAvatar/UserAvatar';
-import thencoLogoBlack from '../../common/assets/images/thenco-logo-black.png';
-import thencoLogoWhite from '../../common/assets/images/thenco-logo-white.jpg';
-import './Nav.scss';
+import UserAvatar from "../UserAvatar/UserAvatar";
+import thencoLogoBlack from "../../common/assets/images/thenco-logo-black.png";
+import thencoLogoWhite from "../../common/assets/images/thenco-logo-white.jpg";
+import "./Nav.scss";
 
 const theme = {
-  value: 'dark'
-}
+  value: "dark"
+};
 
 export default function Nav() {
   return (
     <nav className='nav'>
       <div className='nav__logo'>
         <img
-          src={(theme.value === 'dark') ? thencoLogoWhite : thencoLogoBlack}
+          src={theme.value === "dark" ? thencoLogoWhite : thencoLogoBlack}
           alt='thenco logo'
           className='nav__logo-img'
         />
       </div>
-      <div className='nav__link'>
-        <Link>
-          <div className='nav__link-item'>about thenco</div>
-        </Link>
-        <UserAvatar />
+      <div className='nav__right'>
+        <Link className='nav__link-item'>about thenco</Link>
+        <UserAvatar className='nav__link-item' />
       </div>
     </nav>
   );
