8788df7c3a7715ffbbdafbf5e834a81a069bf427
Angus Ryer <angusryer@MBAe18FVFYN0DSJK77.local>
2020-08-20 21:02:23 -0400

    partially configured gatsby.config.js

---
+++ gatsby.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+      title: `thenco`,
+      siteUrl: `https://www.thenco.com`,
+      description: `thenco curates awesome and makes amazing`,
+    },
+    plugins: [
+      ],
+  }
